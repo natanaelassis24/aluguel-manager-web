e734de8b3e7afaa7ae6a2eb970a8f1ff29c42700
natanael <natanaelassis77@gmail.com>
2025-10-21 16:17:44 -0300

    0

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#0d47a1" />
+      <meta name="description" content="Sistema de gerenciamento de aluguéis" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      {/* Você pode adicionar mais meta tags se quiser */}
+    </>
+  );
+}
